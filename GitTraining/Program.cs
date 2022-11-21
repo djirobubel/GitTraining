7ce98b1c5bdf296366a5629190b79a1d2ef6f845
David <www.beljas007@gmail.com>
2022-11-21 20:44:21 +0300

    Method PrintInfo has been used

--- GitTraining/Program.cs
+++ GitTraining/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using GitTraining;
-Person david = new("David", "Uzunyan", 21);  
+Person david = new("David", "Uzunyan", 21);
+david.PrintInfo();
+
